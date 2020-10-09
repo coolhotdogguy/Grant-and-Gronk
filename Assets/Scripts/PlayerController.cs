3b963dfafdaf70106c7a731b2e58e5ec9ba4ab99
coolhotdogguy <68834792+coolhotdogguy@users.noreply.github.com>
2020-10-09 14:38:55 -0700

    Added player controller and test scene
    player controller not working. It is trying to use old input manager

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player")]
+    [SerializeField] float jumpVelocity = 5f;
+    [SerializeField] float playerSpeed = 5f;
+
+    [Header("Jump Tuning")]
+
+
+    //Awake
+    Rigidbody2D rb2d;
+    Collider2D coll;
+
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpVelocity);
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * 0.5f); //Slows jump velocity if jump button is released
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0) //Right is pressed
+        {
+            rb2d.velocity = new Vector2(playerSpeed, rb2d.velocity.y);
+        }
+        if (Input.GetAxisRaw("Vertical") < 0) //Left is pressed
+        {
+            rb2d.velocity = new Vector2(-playerSpeed, rb2d.velocity.y);
+        }
+    }
+}
