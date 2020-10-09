87a8dec0ecc8aa8597aa1686f5a5884023488229
ParicBat <paric.cz@email.cz>
2020-10-10 00:35:19 +0200

    Animations,, Menu progress and ButtonUtils

---
+++ Assets/Scripts/ButtonUtils.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonUtils : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    public void PlayAnimation(string state)
+    {
+        animator.Play(state);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void SwitchRoom(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void SwitchRoom(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
