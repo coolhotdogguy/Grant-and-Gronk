04cf6c955087c6dc8f84ef8b173066da13b499d7
coolhotdogguy <68834792+coolhotdogguy@users.noreply.github.com>
2020-10-10 13:32:18 -0700

    more tweaks to player controls
    in this iteration, jump isn't a bool but an int. Coyote time and jump button release decelerate are working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,11 +11,11 @@ public class PlayerController : MonoBehaviour
     [Header("Jump Tuning")]
     [SerializeField] float jumpVelocity = 5f;
     [SerializeField] float jumpButtonReleaseDeceleration = 0.5f;
-    [SerializeField] float fallMultiplier = 2.5f;
-    float jumpForgivenessBuffer = 0.1f; //allows jump a little before player lands
-    float jumpForgivenessBufferTimer;
-    float coyoteTime = 0.2f; //extra time to jump once platform is left
-    float coyoteTimeTimer;
+    float jumpForgivenessBuffer = 0.1f; //allows jump input a little before player lands
+    float jumpForgivenessBufferTimer; 
+    [SerializeField] float coyoteTime = 0.2f; //extra time to jump once platform is left
+    float coyoteTimeTimer; //counts down when player leaves a ledge
+    // [SerializeField] float fallMultiplier = 2.5f; was used for faster fall after peak of jump
 
     [Header("Ground")]
     public LayerMask ground;
@@ -25,8 +25,8 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb2d;
     Collider2D coll;
 
-    float move;
-    bool jumping;
+    float move; 
+    int jump;
 
 
     private void Awake()
@@ -37,7 +37,7 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        isGrounded = coll.IsTouchingLayers(ground);
+        isGrounded = coll.IsTouchingLayers(ground); 
 
         HandleCoyoteTime();
 
@@ -50,16 +50,19 @@ public class PlayerController : MonoBehaviour
     {
         rb2d.velocity = new Vector2(playerSpeed * move, rb2d.velocity.y); //Horizontal movemnet
 
-        if (coyoteTimeTimer > 0f && jumpForgivenessBufferTimer > 0f)
+        if (jump == 1 && coyoteTimeTimer > 0f) //if jump pressed and jump still allowed
         {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpVelocity);
-            jumpForgivenessBufferTimer = 0;
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpVelocity); //jump
+        }
+        if (jump == 0 && rb2d.velocity.y > 0) //if jump is released while player is moving up
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * jumpButtonReleaseDeceleration); //slowdown jump velocity
         }
 
-        if (rb2d.velocity.y < 0) //if falling, fall even faster
+        /*if (rb2d.velocity.y < 0) //if falling, fall even faster
         {
             rb2d.velocity += Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime; //-1 to offset unity's gravity
-        }
+        }*/
 
     }
 
@@ -70,22 +73,12 @@ public class PlayerController : MonoBehaviour
 
     void OnJump(InputValue value)
     {
-        jumping = value.isPressed;
-
-        /*if (coyoteTimeTimer > 0f)
-        {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpVelocity);
-        }*/
-    }
-
-    void OnJumpReleased()
-    {
-        rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * jumpButtonReleaseDeceleration);
+        jump = value.Get<float>(); // jumpPressed == 1f, jumpReleased == 0f
     }
 
     private void HandleJumpForgivenessBuffer()
     {
-        if (jumping)
+        if (jump == 1)
         {
             jumpForgivenessBufferTimer = jumpForgivenessBuffer;
         }
