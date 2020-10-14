a5afcdb225f9b50e298fdc4a451558ff05bf10a1
ParicBat <paric.cz@email.cz>
2020-10-14 02:10:56 +0200

    Control binding (yay!)

---
+++ Assets/Scripts/BindControlsButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BindControlsButton : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI text;
+    public InputActionReference mainAction;
+    public int index;
+
+    InputActionRebindingExtensions.RebindingOperation rebindOperation;
+
+    public void Remap(string part)
+    {
+        text.text = "Press a key";
+
+        rebindOperation = mainAction.action.PerformInteractiveRebinding(index)
+                    // To avoid accidental input from mouse motion
+                    .WithControlsExcluding("Mouse")
+                    .WithCancelingThrough("<Keyboard>/escape")
+                    .WithTargetBinding(1)
+                    .OnMatchWaitForAnother(0.2f)
+                    .Start()
+                    .OnApplyBinding(
+                        (op, path) =>
+                        {
+                            text.text = path;
+                        });
+    }
+}
