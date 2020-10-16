bd3e61d7c1307c1c41d3fe9a1c220ef62fd831e8
ParicBat <paric.cz@email.cz>
2020-10-17 01:18:50 +0200

    Animated Temporal Coagulate

---
+++ Assets/Scripts/Utils/RandomizeAnimations.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeAnimations : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] string state;
+
+    void Start()
+    {
+        animator.Play(state, 0, Random.Range(0f, 1f));
+    }
+}
